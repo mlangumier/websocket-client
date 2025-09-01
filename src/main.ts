6e303bfc0ecb4579c324736540c09c2470cda438
Malak Bouaker <bouaker.malak@outlook.fr>
2025-09-01 10:50:09 +0200

    tmp

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,5 @@ let currentPlayer;
 
 
 
-cells.forEach((cell, index)=>{
-  console.log("case cliqué : ", index)
-})
-socket.onopen = () => {
-  console.log("✅ Connecté au serveur WebSocket");
-}
-
-socket.onerror = (err) => {
-    console.error("❌ Erreur WebSocket :", err);
-};
-
-socket.onclose = () => {
-    console.warn("⚠️ Connexion fermée");
-};
 
 
