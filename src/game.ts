b40991cb28a56cab24d806422e426e24aef23e57
Malak Bouaker <bouaker.malak@outlook.fr>
2025-09-02 10:37:23 +0200

    refactor: merge game and main logic, update message structure

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,29 @@
 
+import type { IServerMessage } from "./interfaces/types";
 
+const cells = document.querySelectorAll<HTMLAnchorElement>(".btn")
 
+export function renderBoard(board: string[]){
+  board.forEach((symbol,index ) => {
+    cells[index].textContent = symbol || ""; // met "X", "O" ou vide
+  });
+}
 
+export function handleGameMessage(msg: IServerMessage){
+  if(msg.content.board) renderBoard(msg.content.board);
 
+  switch (msg.type){
+    case "GAME_START":
+      console.log("Game Start !")
+      break;
+      case "UPDATE":
+      console.log("Update board")
+      break;
+      case "GAME_OVER":
+      console.log(msg.content.winner ? `Winner : ${msg.content.winner}` : "Draw")
+      break;
+      case "ERROR":
+      console.error("Server Error : ", msg.content.message);
+      break;
+  }
+}
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,15 +1,42 @@
+import type { IMessage, IServerMessage } from "./interfaces/types";
 
-const socket = new WebSocket(import.meta.env.SERVER_URL);
-
+export const socket = new WebSocket(import.meta.env.SERVER_URL);
 
 socket.onopen = () => {
   console.log("✅ Connecté au serveur WebSocket");
-}
+};
 
-socket.onerror = (err) => {
-    console.error("❌ Erreur WebSocket :", err);
+socket.onerror = err => {
+  console.error("❌ Erreur WebSocket :", err);
 };
 
 socket.onclose = () => {
-    console.warn("⚠️ Connexion fermée");
+  console.warn("⚠️ Connexion fermée");
+};
+
+//  Fonction pour envoyer un message
+export function sendMessage(msg: IMessage) {
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify(msg));
+  } else {
+    console.warn("⏳ WebSocket not ready, message ignored :", msg);
+  }
+}
+
+//  Callback typé pour écouter les messages serveur
+type ServerCallback = (msg: IServerMessage) => void;
+let messageCallback: ServerCallback | null = null;
+
+export function onMessage(callback: ServerCallback) {
+  messageCallback = callback;
+}
+
+//  Réception des messages
+socket.onmessage = event => {
+  try {
+    const data: IServerMessage = JSON.parse(event.data);
+    if (messageCallback) messageCallback(data);
+  } catch (err) {
+    console.error("❌ Error parsing WebSocket:", err);
+  }
 };
