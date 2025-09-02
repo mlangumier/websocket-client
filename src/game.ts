20743e4301ee795fb168a806de770d5ea310b78f
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-09-02 16:56:34 +0200

    Stuck: click btn & display board

--- src/game.ts
+++ src/game.ts
@@ -1,39 +1,57 @@
-import type { IServerMessage } from "./interfaces/types";
+import type { IPlayer, IServerMessage } from "./interfaces/types";
 import { sendMessage } from "./websocket";
 
 const cells = document.querySelectorAll<HTMLAnchorElement>(".btn");
-
-export function renderBoard(board: string[]) {
-  board.forEach((symbol, index) => {
-    cells[index].textContent = symbol || ""; // met "X", "O" ou vide
-  });
-}
+let player: IPlayer | null = null;
+let gameData: IServerMessage = {
+  type: "ERROR",
+  players: {
+    player1: null,
+    player2: null,
+  },
+  board: new Array<string | null>(9).fill(null),
+  currentTurn: null,
+};
 
 cells.forEach((cell, index) => {
   cell.addEventListener("click", () => {
-    console.log("Click btn: " + index);
-    // sendMessage to server {type, index}
+    sendMessage({ type: "MOVE", content: index.toString() });
   });
 });
 
 export function handleGameMessage(msg: IServerMessage) {
-  if (msg.content.board) renderBoard(msg.content.board);
-  console.log(msg);
+  gameData = { ...msg };
+  if (msg.board) renderBoard(msg.board); //ERROR: always same symbol, fix SERVER!!
+
+  // Get & render game board
 
   switch (msg.type) {
+    case "PLAYER_INFO": {
+      // Only the player who entered their name receives msg.message
+      if (msg.message?.symbol) {
+        player = msg.message; // Player's name & symbol
+      }
+      break;
+    }
     case "GAME_START":
       console.log("Game Start !");
+      // Get msg.currentPlayer & remove btn.disabled for this player if player.name === msg.currentPlayer
       break;
     case "UPDATE":
       console.log("Update board");
       break;
     case "GAME_OVER":
-      console.log(
-        msg.content.winner ? `Winner : ${msg.content.winner}` : "Draw"
-      );
+      console.log(msg.winner ? `Winner : ${msg.winner}` : "Draw");
+      // Display alert (game over + info)
       break;
     case "ERROR":
-      console.error("Server Error : ", msg.content.message);
+      console.error("Server Error : ", msg.message);
       break;
   }
+}
+
+export function renderBoard(board: (string | null)[]) {
+  board.forEach((symbol, index) => {
+    cells[index].textContent = symbol || ""; // met "X", "O" ou vide
+  });
 }
